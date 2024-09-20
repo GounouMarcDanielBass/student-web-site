64cf2bf527e36b9041c8edbccb9323daa56f947a
Gounou Marc Daniel Bass <marcdanielbassgounou@gmail.com>
2024-09-20 11:51:15 +0100

    jhgiukyhijk

--- server/server.js
+++ server/server.js
@@ -4,14 +4,11 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 
-// Middleware
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from the React frontend
-app.use(express.static(path.join(__dirname, "public"))); // Adjust this if your build folder is named differently
-
-// Database configuration
+const port = 5000;
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -19,27 +16,11 @@ const db = mysql.createConnection({
   database: "students",
 });
 
-// Connect to MySQL
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed: " + err.stack);
-    return;
-  }
-  console.log("Connected to database.");
-});
-
-// API endpoints
-
 // Add a new user
 app.post("/add_user", (req, res) => {
   const sql =
-    "INSERT INTO student_details (`name`, `email`, `age`, `gender`) VALUES (?, ?, ?, ?)";
-  const values = [
-    req.body.name,
-    req.body.email,
-    req.body.age,
-    req.body.gender,
-  ];
+    "INSERT INTO student_details (`name`,`email`,`age`,`gender`) VALUES (?, ?, ?, ?)";
+  const values = [req.body.name, req.body.email, req.body.age, req.body.gender];
   db.query(sql, values, (err, result) => {
     if (err)
       return res.json({ message: "Something unexpected has occurred: " + err });
@@ -112,13 +93,8 @@ app.post("/custom_search", (req, res) => {
   });
 });
 
-// Serve the React app for any other route
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html")); // Adjust this if your build folder is named differently
-});
 
 // Start the server
-const port = process.env.PORT || 5000; // Use environment variable for port
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
